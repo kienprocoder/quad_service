8525c8fa033decaaecdd2387b01beb4102ab9b2d
jackie chul <jackiechul143@gmail.com>
2023-06-21 22:39:37 +0700

    first commit

---
+++ src/main/java/vn/itech/com/quad_service/QuadServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.itech.com.quad_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/itech/com/quad_service/QuadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.itech.com.quad_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
